6db1887bc86b32bc17962cff7782a3a3f6936b7e
Aisha Umar <igotwebmail@gmail.com>
2026-02-26 01:13:13 -0500

    twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -1,25 +1,20 @@
 function hasPair(arr, target) {
-  let left 
-  let right
-  let sum 
-
-  for(let i of arr){
-    left = arr[i]
-    right = arr[length-1-i]
-    sum = left + right
-    if(sum == target){
-        return true
-    }
-    else if(sum > target){
-        i = i-1
-        right = arr[-i]
+  let left = 0
+  let right = arr.length - 1
+  
+  while(left < right){
+  let sum = arr[left] + arr[right]
+    if(sum === target){
+      return true
     }
     else if(sum < target){
-        i = i + 1
-        left = arr[i]
+      left++
+    }
+    else{
+      right++
     }
   }
-
+  return false
 }
 
 console.log(hasPair([1, 2, 3, 4, 6],6))
