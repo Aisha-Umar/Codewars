fab442b1c44f29a9a76ccfadefb8918609edd972
Aisha Umar <igotwebmail@gmail.com>
2026-03-01 15:17:58 -0500

    duplicate with Set

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -5,3 +5,24 @@ function removeDupes(arr) {
 console.log(removeDupes([1,2,2,3])) 
 console.log(removeDupes(["a","a","b"],["a","b"]))
 console.log(removeDupes([1,1,1,1],[1]))
+
+function removeDuplicates(arr){
+    let obj ={}
+
+    for(let num of nums){
+        //check if num is already in the obj
+        if(obj[num]){
+            return num
+        }
+            obj[num] = 1
+    }
+}
+ 
+    let seen = new Set()
+
+    for(let num of nums){
+        if(seen.has(num)){
+            return num
+        }
+        seen.add(num)
+    }
